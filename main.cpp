e23124c996cf33721a12d9c6ad92166395769a05
ciriast <ciriastorres@gmail.com>
2024-11-16 20:42:13 -0600

    feat: Add allocator file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "memory_management/allocator.h"
 
 int main() {
 	std::cout << "Hello World" << std::endl;
+	printMessage();
+
 	return 0;
 }
---
+++ memory_management/allocator.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void printMessage() {
+	std::cout << "Hello from Allocator" <<std::endl;
+}
