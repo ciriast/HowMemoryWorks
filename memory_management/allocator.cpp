08a1a328f9e6d58df6c1e9509a81b010979f9fa8
ciriast <ciriastorres@gmail.com>
2024-11-19 06:05:43 -0600

    doc: Add comments to the stack example

--- memory_management/allocator.cpp
+++ memory_management/allocator.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+// Let's learn about the stack
+// For temporary variables
+// If we know the length in compilation time
+// Is very fast
+// The memory management is automatic when we left the scope
+// The length is limited and is tipically is used for local variables
 void stackExample() {
 	int stackVar = 42; // A variable in the stack
 	std::cout << "Value in the stack: " << stackVar <<std::endl;
